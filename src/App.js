75a985b86b1c6d5ab0867f6a71dd4f608bea0ff5
Nguyenminhhoang952000 <codon1visao952000@gmail.com>
2023-02-02 10:05:21 +0700

    header,footer

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from 'components/header/Header';
+import Footer from 'components/footer/Footer';
+import Routes from 'config/Routes';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes />
+            <Footer />
+        </BrowserRouter>
+    );
+}
+
+export default App;
